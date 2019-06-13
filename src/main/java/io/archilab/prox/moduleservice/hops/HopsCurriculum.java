ffdb1aa9d4439caa218923527c6d9dc4f6986ba2
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-06-13 15:19:57 +0200

    Refactor HopsCurriculum to include all properties

--- src/main/java/io/archilab/prox/moduleservice/hops/HopsCurriculum.java
+++ src/main/java/io/archilab/prox/moduleservice/hops/HopsCurriculum.java
@@ -10,14 +10,14 @@ import lombok.ToString;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class HopsCurriculum {
 
-  // @JsonProperty("ZUORDNUNG_CURRICULUM")
-  // private String ZUORDNUNG_CURRICULUM;
+  @JsonProperty("ZUORDNUNG_CURRICULUM")
+  private String ZUORDNUNG_CURRICULUM;
 
   @JsonProperty("SG_KZ")
   private String SG_KZ;
 
-  // @JsonProperty("SR_KZ")
-  // private String SR_KZ;
+  @JsonProperty("SR_KZ")
+  private String SR_KZ;
 
   @JsonProperty("SEMESTER")
   private String SEMESTER;
